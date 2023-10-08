48b366056a537f8788392ebbaf91339f5d3b1306
geobde <geobde@gmail.com>
2023-10-08 23:40:22 +0300

    update text

--- src/app/page.js
+++ src/app/page.js
@@ -36,7 +36,7 @@ export default function Home() {
                           onChange={handleInputChange}
                           rows="1" 
                           class="flex-[1_0_50%] min-w-[50%] disabled:opacity-80 text-white bg-transparent border-0 shadow-none resize-none outline-none ring-0 disabled:bg-transparent selection:bg-teal-300 selection:text-black placeholder:text-zinc-400 [scroll-padding-block:0.75rem] pl-3 py-3 sm:min-h-[15px] sm:leading-6 text-base md:text-sm" 
-                          placeholder="What’s your GraphQL API Endpoint?" 
+                          placeholder="Type something" 
                           style={{height: "41px !important"}} 
                         />
                     </div>
