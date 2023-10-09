71241e1dbd36fd2365aaac51dc3f4f55aced6bdd
geobde <geobde@gmail.com>
2023-10-09 05:27:33 +0300

    add constant

--- src/app/api/chat/route.js
+++ src/app/api/chat/route.js
@@ -1,6 +1,8 @@
 import OpenAI from 'openai';
 import { OpenAIStream, StreamingTextResponse } from 'ai';
 
+const MODEL = "gpt-3.5-turbo";
+
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
@@ -12,7 +14,7 @@ export async function POST(req) {
   const { endpoint, messages } = await req.json();
 
   const response = await openai.chat.completions.create({
-    model: 'gpt-3.5-turbo',
+    model: MODEL,
     stream: true,
     messages
   });
